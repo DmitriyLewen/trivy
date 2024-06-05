2e529a2c27d69c9ad95d52e68359ec149b4e8285
DmitriyLewen <dmitriy.lewen@smartforce.io>
2024-06-05 12:05:56 +0600

    fix: test 6

--- pkg/commands/app_test.go
+++ pkg/commands/app_test.go
@@ -173,7 +173,7 @@ func testPBackport(s string, t *testing.T) {
 }
 
 func TestFlags(t *testing.T) {
-	testPBackport("pr5", t)
+	testPBackport("pr6", t)
 	type want struct {
 		format     types.Format
 		severities []dbTypes.Severity
