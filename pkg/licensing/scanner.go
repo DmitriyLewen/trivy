1e38aabcb75c1eedc1f76b1decc9878652be8452
DmitriyLewen <dmitriy.lewen@smartforce.io>
2025-05-15 13:09:52 +0600

    fix: add visited func to avoid overwriting

--- pkg/licensing/scanner.go
+++ pkg/licensing/scanner.go
@@ -6,6 +6,7 @@ import (
 	dbTypes "github.com/aquasecurity/trivy-db/pkg/types"
 	"github.com/aquasecurity/trivy/pkg/fanal/types"
 	"github.com/aquasecurity/trivy/pkg/licensing/expression"
+	"github.com/aquasecurity/trivy/pkg/log"
 )
 
 type ScannerOption struct {
@@ -22,21 +23,92 @@ func NewScanner(categories map[types.LicenseCategory][]string) Scanner {
 }
 
 func (s *Scanner) Scan(licenseName string) (types.LicenseCategory, string) {
-	normalized := NormalizeLicense(expression.SimpleExpr{License: licenseName})
-	var normalizedName string
-	switch normalized := normalized.(type) {
-	case expression.SimpleExpr:
-		normalizedName = normalized.License
-	case expression.CompoundExpr:
-		normalizedName = normalized.String()
+	visited := map[string]types.LicenseCategory{}
+	category := s.traverseLicenseExpression(licenseName, visited)
+
+	return category, categoryToSeverity(category).String()
+}
+
+// traverseLicenseExpression recursive parses license expression to detect correct license category:
+// For Simple Expression - use category of license
+// For Compound Expression:
+//   - `AND` operator - use category with maximum severity
+//   - `OR` operator - use category with minimum severity
+//   - one of expression has `UNKNOWN` category - use `UNKNOWN` category
+func (s *Scanner) traverseLicenseExpression(licenseName string, visited map[string]types.LicenseCategory) types.LicenseCategory {
+	category := types.CategoryUnknown
+	detectCategoryAndSeverity := func(expr expression.Expression) expression.Expression {
+		// Skip if we already check this license
+		if cat, ok := visited[licenseName]; ok {
+			category = cat
+			return expr
+		}
+
+		switch e := expr.(type) {
+		case expression.SimpleExpr:
+			category = s.licenseCategory(e.License)
+		case expression.CompoundExpr:
+			category = s.compoundLicenseToCategory(e, visited)
+		}
+
+		visited[licenseName] = category
+		return expr
 	}
 
+	_, err := expression.Normalize(licenseName, NormalizeLicense, detectCategoryAndSeverity)
+	if err != nil {
+		log.WithPrefix("license").Debug("Unable to detect license category", log.String("license", licenseName), log.Err(err))
+		return types.CategoryUnknown
+	}
+
+	return category
+}
+
+func (s *Scanner) compoundLicenseToCategory(license expression.CompoundExpr, visited map[string]types.LicenseCategory) types.LicenseCategory {
+	switch license.Conjunction() {
+	case expression.TokenAnd:
+		return s.compoundLogicOperator(license, visited, true)
+	case expression.TokenOR:
+		return s.compoundLogicOperator(license, visited, false)
+	default:
+		return types.CategoryUnknown
+	}
+}
+
+func (s *Scanner) compoundLogicOperator(license expression.CompoundExpr, visited map[string]types.LicenseCategory, findMax bool) types.LicenseCategory {
+	lCategory := s.traverseLicenseExpression(license.Left().String(), visited)
+	lSeverity := categoryToSeverity(lCategory)
+	rCategory := s.traverseLicenseExpression(license.Right().String(), visited)
+	rSeverity := categoryToSeverity(rCategory)
+
+	if lSeverity == dbTypes.SeverityUnknown || rSeverity == dbTypes.SeverityUnknown {
+		return types.CategoryUnknown
+	}
+
+	var logicOperator int
+	if findMax {
+		logicOperator = 1
+	} else {
+		logicOperator = -1
+	}
+
+	var compoundCategory types.LicenseCategory
+	if 0 < logicOperator*dbTypes.CompareSeverityString(lSeverity.String(), rSeverity.String()) {
+		compoundCategory = rCategory
+	} else {
+		compoundCategory = lCategory
+	}
+
+	return compoundCategory
+}
+
+func (s *Scanner) licenseCategory(licenseName string) types.LicenseCategory {
 	for category, names := range s.categories {
-		if slices.Contains(names, normalizedName) {
-			return category, categoryToSeverity(category).String()
+		if slices.Contains(names, licenseName) {
+			return category
 		}
 	}
-	return types.CategoryUnknown, dbTypes.SeverityUnknown.String()
+	return types.CategoryUnknown
 }
 
 func categoryToSeverity(category types.LicenseCategory) dbTypes.Severity {
